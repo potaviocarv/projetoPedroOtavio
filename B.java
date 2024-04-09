c98ea44d6166c575945809963946a2eefc407b53
Pedro Nunes <potaviocarv@gmail,com>
2024-04-09 15:32:02 -0300

    Adicionadas classes A e B com getters, setters e métodos

---
+++ B.java
@@ -0,0 +1,37 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    // Construtor
+    public B(int B1, float B2) {
+        this.B1 = B1;
+        this.B2 = B2;
+    }
+
+    // Getters e Setters
+    public int getB1() {
+        return B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+    // Método MB1
+    public void MB1() {
+        System.out.println("Nome do método: MB1()");
+    }
+
+    // Método MB2
+    public void MB2() {
+        System.out.println("Nome do método: MB2()");
+    }
+}
